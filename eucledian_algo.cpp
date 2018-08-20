e22d145b2ff221271cef481a456af63f05ff3887
gitu-m <cs16btech11014@iith.ac.in>
2018-08-20 14:14:04 +0530

    Edit files

--- eucledian_algo.cpp
+++ eucledian_algo.cpp
@@ -1,17 +1,14 @@
-// C program to demonstrate Basic Euclidean Algorithm
 #include <stdio.h>
  
-// Function to return gcd of a and b
-int gcd(int a, int b)
-{
+int gcd(int a, int b){
+
     if (a == 0)
         return b;
     return gcd(b%a, a);
 }
  
-// Driver program to test above function
-int main()
-{
+int main(){
+    
     int a = 10, b = 15;
     printf("GCD(%d, %d) = %dn", a, b, gcd(a, b));
     a = 35, b = 10;
--- fib_series.cpp
+++ fib_series.cpp
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int fib(int n)
-{
+int fib(int n){
+
   int a = 0, b = 1, c, i;
   if( n == 0)
     return a;
-  for (i = 2; i <= n; i++)
-  {
+  for (i = 2; i <= n; i++){
+
      c = a + b;
      a = b;
      b = c;
   }
+
   return b;
 }
  
-int main ()
-{
+int main (){
+
   int n;
   scanf("%d", &n);
   printf("%d", fib(n));
